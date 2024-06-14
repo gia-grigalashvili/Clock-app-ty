dac6754c06d42b10010cc444a49f7aed6ed6bab7
gia-grigalashvili <grigalagia07@gmail.com>
2024-06-14 16:49:59 +0400

    made header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+interface GlobalStyleProps {
+  isDark: boolean;
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDark, setIsDark] = useState<boolean>(false);
+  const [timeData, setTimeData] = useState<any>(null);
+  const toggleDarkMode = () => {
+    setIsDark(!isDark);
+  };
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await fetch("https://api.quotable.io/random");
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const data = await response.json();
+  //       setTimeData(data);
+  //     } catch (error) {
+  //       console.error("Failed to fetch data:", error);
+  //     }
+  //   })();
+  // }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyle isDark={isDark} />
     </>
-  )
+  );
 }
 
-export default App
+const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
+  body {
+    background-color: ${(props) =>
+      props.isDark
+        ? "/public/assets/mobile/bg-image-daytime.jpg"
+        : "/public/assets/mobile/bg-image-daytime.jpg"};
+  }
+`;
+export default App;
